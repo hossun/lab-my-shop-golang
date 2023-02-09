f2a1116759a0f4e31bb089ec93906686a7e0fb4b
경호선 <hossun73@gmail.com>
2023-02-09 13:21:04 +0900

    commit

---
+++ delivery/delivery/PolicyHandler.go
@@ -0,0 +1,14 @@
+package delivery
+
+import (
+	"github.com/mitchellh/mapstructure"
+)
+
+func wheneverOrderPlaced_AddToDeliveryList(data map[string]interface{}){
+	
+	event := NewOrderPlaced()
+	mapstructure.Decode(data,&event)
+
+	AddToDeliveryList(event);
+}
+
---
+++ inventory/inventory/PolicyHandler.go
@@ -0,0 +1,22 @@
+package inventory
+
+import (
+	"github.com/mitchellh/mapstructure"
+)
+
+func wheneverOrderPlaced_DecreaseStock(data map[string]interface{}){
+	
+	event := NewOrderPlaced()
+	mapstructure.Decode(data,&event)
+
+	DecreaseStock(event);
+}
+
+func wheneverOrderCancelled_IncreaseStock(data map[string]interface{}){
+	
+	event := NewOrderCancelled()
+	mapstructure.Decode(data,&event)
+
+	IncreaseStock(event);
+}
+
